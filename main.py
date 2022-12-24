89c81cdbbb031e5fab178c7bbd48c8b0f4abeb74
Diego Beltran <diego.beltrangg24@gmail.com>
2022-12-24 13:36:46 -0800

    Second commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import modules.db.modules as db
+from datetime import datetime
+
 
 
 def run():
-    pass
+    db.db_init()
+    db.create_rol()
+
 
 if __name__ =="__main__":
     run()
+
+
+
+def create_user():
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    message = "hola"
+    db.User(name="Diego Beltran",email="diego_beltran123@live.com",password=fernet.encrypt(message.encode()),rol="",date_created=datetime.now())
+    #commit()
--- modules/db/modules.py
+++ modules/db/modules.py
@@ -1,5 +1,7 @@
+import datetime
 from pony.orm import *
 from decouple import config
+from cryptography.fernet import Fernet
 
 DB_USERNAME = config('DB_USERNAME')
 DB_HOST = config('DB_HOST')
@@ -7,10 +9,41 @@ DB_PASSWORD = config('DB_PASSWORD')
 DB_NAME = config('DB_NAME')
 
 db = Database()
+db.bind(provider='mysql', host = DB_HOST, user = DB_USERNAME, passwd = DB_PASSWORD, db = DB_NAME,ssl={"verify_mode": None})
 
-db.bind(provider='mysql', host = DB_HOST, user = DB_USERNAME, passwd = DB_PASSWORD, db = DB_NAME)
+set_sql_debug(True)
 
-class Person(db.Entity):
-    name = Required(str)
-    age = Required(int)
-    cars = Set('Car')
+class User(db.Entity):
+    id = PrimaryKey(int,auto=True)
+    name = Required(str,max_len=40)
+    email = Required(str,max_len=20)
+    password = Required(str,max_len=15)
+    rol = Optional('Rol')
+    date_created = Required(datetime.datetime,6)
+
+class Rol(db.Entity):
+    id = PrimaryKey(int,auto=True)
+    name = Required(str,max_len=40)
+    user =  Set(User)
+
+class Champion(db.Entity):
+    id = PrimaryKey(int,auto=True)
+    name = Required(str,max_len=100)
+
+@db_session
+def create_rol():
+    db.Rol(name="User")
+    print("Rol Created Succesfully :)")
+
+@db_session
+def create_User():
+    
+    db.Rol(name="User")
+    print("User Created Succesfully :)")
+
+#First time created DB
+def db_init():
+    db.generate_mapping() 
+    #db.create_tables()
+    #db.drop_all_tables(with_all_data=True) 
+    
